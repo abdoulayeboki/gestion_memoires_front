210da00e1c8884505f6ec8ee316d6b54ebc083a7
Abdoulaye SARR <abdoulaye.sarr@uvs.edu.sn>
2021-02-23 13:30:36 +0000

    paramettrage du deploiement

--- serveur.js
+++ serveur.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const path = require("path")
+const path = require('path')
 
 const app = express();
 
 app.use(express.static(__dirname + '/dist/gestion-memoires-front'));
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/gestion-memoires-front/index.html'));
 
 })
