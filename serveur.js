76168a5d73c151cf63dad3fd5ff84c8fa67db5ec
Abdoulaye SARR <abdoulaye.sarr@uvs.edu.sn>
2021-02-23 14:19:06 +0000

    paramettrage du deploiement 1.0

--- serveur.js
+++ serveur.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const path = require('path')
-
-const app = express();
-
-app.use(express.static(__dirname + '/dist/gestion-memoires-front'));
+const app = express()
+app.use(express.static('./dist/angular-gestion-memoires-sonat'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/gestion-memoires-front/index.html'));
+    res.sendFile('index.html', { root: 'dist/angular-gestion-memoires-sonat/' });
+});
 
-})
+app.listen(process.env.PORT || 8080);
 
-app.listen(process.env.PORT)
+// const express = require("express");
+// const path = require('path')
+
+// const app = express();
+
+// app.use(express.static(__dirname + '/dist/gestion-memoires-front'));
+
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/gestion-memoires-front/index.html'));
+
+// })
+
+// app.listen(process.env.PORT)
